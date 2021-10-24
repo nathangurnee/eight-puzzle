7f37c99887a90ec78d9eb9a72e40c34aeb9216f8
nathangurnee <nathangurnee@gmail.com>
2021-10-23 19:26:54 -0700

    Beginning search functionality

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from puzzle import Puzzle
 if __name__ == '__main__':
     puzzle_input = input('Welcome to the 8-Puzzle Solver. Type \'1\' to use a default puzzle, or \'2\' to create your own.\n')
 
-    start_state = [1, 2, 3, 4, 0, 6, 7, 5, 8] # Default state
+    start_state = [1, 2, 3, 4, 5, 6, 7, 0, 8] # Default state
     
     if puzzle_input == '2':
         print('Enter your puzzle, using zero to represent the blank. Please only enter valid 8-puzzles. Enter the puzzle delimiting the numbers with a space. Type RETURN only when finished.\n')
@@ -19,3 +19,4 @@ if __name__ == '__main__':
     alg_input = input('\nSelect algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) for the Manhattan Distance Heuristic.\n')
 
     puzzle = Puzzle(start_state, alg_input)
+    puzzle.solve()
--- puzzle.py
+++ puzzle.py
@@ -1,3 +1,6 @@
+import time
+import heapq
+
 # Functionality of the puzzle
 class Puzzle():
     def __init__(self, start_state, algorithm):
@@ -55,19 +58,19 @@ class Puzzle():
             return None
         self.swap_tile(1)
 
-    # Calculates the cost of the current state of puzzle
-    def cost(self):
+    # Calculates the cost of a puzzle state
+    def cost(self, state):
         state_cost = 0
         if self.algorithm == 'uniform cost':
             pass # The cost of each node is the same
         elif self.algorithm == 'misplaced tile':
             # Keeps track of number of misplaced tiles
-            for index, tile in enumerate(self.current_state):
+            for index, tile in enumerate(state):
                 if tile != self.goal_state[index]:
                     state_cost += 1
         elif self.algorithm == 'manhattan distance':
             # Sum of manhattan distance of all tiles
-            for index, tile in enumerate(self.current_state):
+            for index, tile in enumerate(state):
                 # Uses manhattan distance formula
                 state_cost += abs(index // 3 - self.goal_state.index(tile) // 3) + abs(index % 3 - self.goal_state.index(tile) % 3)
         return state_cost
@@ -108,7 +111,12 @@ class Puzzle():
                 self.current_state[index] = tile
             self.zero_index = original_state.index(0)
 
+        return self.child_states
+
     # Shows the puzzle on each step
+    # time The amount of time the algorithm took to finish
+    # nodes The total number of nodes explored
+    # size The max queue size during the algorithm
     def display(self):
         print('\n')
         print(self.current_state[0:3])
@@ -117,4 +125,29 @@ class Puzzle():
 
     # Handles solving the puzzle using selected algorithm
     def solve(self):
-        search(self.algorithm)
+        initial_time = time.time() # Initializes time
+        frontier = [] # The states to be explored
+        states_seen = set() # The states already explored
+        
+        heapq.heappush(frontier, (self.cost(self.start_state), self.start_state))
+        for i in range(0, 10):
+            self.current_state = heapq.heappop(frontier)[1]
+            states_seen.add(tuple(self.current_state))
+            self.display()
+
+            if self.current_state != self.goal_state:
+                for child in self.children():
+                    heapq.heappush(frontier, (self.cost(child), child))
+
+            i += 1
+
+                
+                
+
+
+
+
+
+
+
+        final_time = time.time() - initial_time # Time to find goal state
